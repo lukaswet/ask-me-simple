b3920229b41a56a26d25cca072c25ef87297b1ba
ЛУКАС <lukaswet@gmail.com>
2017-05-10 14:59:48 +0000

    Question views and some ajax

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170509130022) do
+ActiveRecord::Schema.define(version: 20170510105816) do
+
+  create_table "answers", force: :cascade do |t|
+    t.text     "body"
+    t.integer  "question_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["question_id"], name: "index_answers_on_question_id"
+  end
 
   create_table "questions", force: :cascade do |t|
     t.string   "title"
